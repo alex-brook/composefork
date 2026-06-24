a7c74a99a215ea87670f055586280b4739ad3062
Alex <43730469+alex-brook@users.noreply.github.com>
2026-06-24 21:55:18 +0100

    refactor, down command

--- cmd/worktree.go
+++ cmd/worktree.go
@@ -12,11 +12,39 @@ var worktreeCmd = &cobra.Command{
     Assuming you are in a worktree of a project that defines a compose based
     devcontainer, bring up a fork of that project for use in the worktree
   `,
-	Run: func(cmd *cobra.Command, args []string) {
-		internal.NewForkCommand().Run()
+}
+
+var worktreeUpCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Bring up the compose project for this worktree",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return internal.RunUpCommand()
 	},
 }
 
+var worktreeDownCmd = &cobra.Command{
+	Use:   "down",
+	Short: "Tear down the compose project for this worktree",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return internal.RunDownCommand()
+	},
+}
+
+var worktreePsCmd = &cobra.Command{
+	Use:   "ps",
+	Short: "List compose projects for all worktrees",
+}
+
+var worktreePruneCmd = &cobra.Command{
+	Use:   "prune",
+	Short: "Remove all compose projects for worktrees that no longer exist",
+}
+
 func init() {
+	worktreeCmd.AddCommand(worktreeUpCmd)
+	worktreeCmd.AddCommand(worktreeDownCmd)
+	worktreeCmd.AddCommand(worktreePsCmd)
+	worktreeCmd.AddCommand(worktreePruneCmd)
+
 	rootCmd.AddCommand(worktreeCmd)
 }
---
+++ internal/up.go
@@ -0,0 +1,100 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/compose-spec/compose-go/v2/cli"
+	"github.com/compose-spec/compose-go/v2/types"
+	"github.com/docker/compose/v5/pkg/api"
+)
+
+func RunUpCommand() error {
+	// Initialize docker client
+	_, compose, err := newClient()
+	if err != nil {
+		return fmt.Errorf("error initializing docker client: %w", err)
+	}
+
+	// Resolve parent / master project
+	log.Println("Loading parent project")
+	parent, err := loadProject()
+	if err != nil {
+		return fmt.Errorf("error loading project: %w", err)
+	}
+
+	// Build the master project
+	log.Println("Building", parent.Name)
+	err = compose.Build(context.Background(), parent, api.BuildOptions{})
+	if err != nil {
+		return fmt.Errorf("error building project: %w", err)
+	}
+
+	// Create a child project for the worktree
+	log.Println("Creating fork project")
+	project, err := overrideProject(parent)
+	if err != nil {
+		return fmt.Errorf("error creating project: %w", err)
+	}
+
+	// Bring a child project up
+	log.Println("Bringing up", project.Name)
+	err = compose.Up(context.Background(), project, api.UpOptions{Create: api.CreateOptions{}, Start: api.StartOptions{}})
+	if err != nil {
+		return fmt.Errorf("up error: %w", err)
+	}
+
+	return nil
+}
+
+func overrideProject(parent *types.Project) (*types.Project, error) {
+	name, oldName, err := projectName(parent)
+	if err != nil {
+		return nil, fmt.Errorf("error computing project name: %w", err)
+	}
+
+	opts, err := cli.NewProjectOptions(
+		nil,
+		cli.WithOsEnv,
+		cli.WithEnvFiles(),
+		cli.WithDotEnv,
+		cli.WithConfigFileEnv,
+		cli.WithDefaultConfigPath,
+		cli.WithResolvedPaths(true),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	opts.Environment["COMPOSE_PROJECT_NAME"] = name
+	project, err := opts.LoadProject(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	for _, srv := range project.Services {
+		if srv.Image == "" {
+			srv.Image = fmt.Sprintf("%s-%s:%s", oldName, srv.Name, "latest")
+			srv.Build = &types.BuildConfig{}
+		}
+		for i := range srv.Ports {
+			srv.Ports[i].Published = ""
+			srv.Ports[i].HostIP = ""
+		}
+		if srv.CustomLabels == nil {
+			srv.CustomLabels = types.Labels{}
+		}
+		// Set expected compose labels for this container
+		srv.CustomLabels[api.ProjectLabel] = project.Name
+		srv.CustomLabels[api.ServiceLabel] = srv.Name
+		srv.CustomLabels[api.OneoffLabel] = "False"
+		srv.CustomLabels[api.WorkingDirLabel] = project.WorkingDir
+		srv.CustomLabels[api.ConfigFilesLabel] = strings.Join(project.ComposeFiles, ",")
+
+		project.Services[srv.Name] = srv
+	}
+
+	return project, nil
+}
