6d43669e7a80bb8859677005c31ed55ae4fac3b2
Alex <43730469+alex-brook@users.noreply.github.com>
2026-07-05 20:21:29 +0100

    load volumes from cache

--- internal/cache.go
+++ internal/cache.go
@@ -3,7 +3,7 @@ package internal
 import (
 	"context"
 	"fmt"
-	"io"
+	// "io"
 	"log"
 	"os"
 	"path/filepath"
@@ -59,43 +59,45 @@ func exportVolumes(cli *command.DockerCli, project *types.Project) error {
 		return fmt.Errorf("error copying volumes: %w", err)
 	}
 
-	binds := []string{}
+	binds := []string{fmt.Sprintf("%s:/out", dir)}
+	commands := []string{"sh", "-c", `
+    set -e
+    echo "all args $@"
+    while [ "$#" -ge 2 ]; do
+      echo "copying $1 to $2"
+      tar -c --numeric-owner -f $2 -C $1 .
+      shift 2
+    done
+  `, "foo"}
 	for _, vol := range project.Volumes {
 		binds = append(binds, fmt.Sprintf("%s:/in/%s:ro", vol.Name, vol.Name))
+		inputPath := filepath.Join("/in", vol.Name)
+		outputPath := filepath.Join("/out", fmt.Sprintf("%s.tar", vol.Name))
+		commands = append(commands, inputPath, outputPath)
 	}
 
 	id, err := createSystemContainer(cli, client.ContainerCreateOptions{
 		Config: &container.Config{
-			Cmd: []string{"sleep", "infinity"},
+			Cmd: commands,
 		},
 		HostConfig: &container.HostConfig{
-			Binds: binds,
+			Binds:      binds,
+			AutoRemove: true,
+			UsernsMode: "host",
 		},
 	})
 	if err != nil {
 		return err
 	}
 
-	for _, vol := range project.Volumes {
-		log.Println("Caching", vol.Name)
-		result, err := cli.Client().CopyFromContainer(context.Background(), id, client.CopyFromContainerOptions{
-			SourcePath: filepath.Join("/", "in", vol.Name),
-		})
-		if err != nil {
-			return fmt.Errorf("error copying volume %s: %w", vol.Name, err)
+	waitResult := cli.Client().ContainerWait(context.Background(), id, client.ContainerWaitOptions{Condition: container.WaitConditionNotRunning})
+	select {
+	case err := <-waitResult.Error:
+		return err
+	case status := <-waitResult.Result:
+		if status.StatusCode != 0 {
+			return fmt.Errorf("container exited with code %d", status.StatusCode)
 		}
-
-		out, err := os.Create(filepath.Join(dir, fmt.Sprintf("%s.tar", vol.Name)))
-		if err != nil {
-			return fmt.Errorf("error copying volume %s: %w", vol.Name, err)
-		}
-		defer out.Close()
-		io.Copy(out, result.Content)
-	}
-
-	_, err = cli.Client().ContainerRemove(context.Background(), id, client.ContainerRemoveOptions{Force: true})
-	if err != nil {
-		return fmt.Errorf("error copying volumes: %w", err)
 	}
 
 	log.Println("Finished caching")
