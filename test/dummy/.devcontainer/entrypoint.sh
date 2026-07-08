e7277c89267c4390365039e8b2a0993368a94c59
Alex <43730469+alex-brook@users.noreply.github.com>
2026-07-08 21:52:16 +0100

    setup gemstash for tests

--- test/dummy/.devcontainer/entrypoint.sh
+++ test/dummy/.devcontainer/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-bundle config set mirror.https://rubygems.org http://host.docker.internal:9292
+bundle config set mirror.https://rubygems.org http://stash:9292
 bundle install
 ./bin/rails db:prepare
 
